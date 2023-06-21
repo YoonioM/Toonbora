dafdb9c69a3364565c46885539ffc833d3aedc17
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2023-06-21 23:16:04 +0900

    feat & fix /  내비 숨기기 기능 구현, 페이지 표기 오류 수정

--- src/components/ViewerFooter.tsx
+++ src/components/ViewerFooter.tsx
@@ -8,28 +8,22 @@ export default function ViewerFooter() {
 
     return (
         <>
-            {/* <View className='w-full bg-slate-50' style={{
-                shadowColor: "#000",
-                shadowOffset: {
-                    width: 0,
-                    height: 1,
-                },
-                shadowOpacity: 0.20,
-                shadowRadius: 2,
-            }}/> */}
             <View className='flex flex-row justify-start z-20'>
-                <View className='bg-purple-500 h-1 w-1/2 relative z-10' style={
-                    { width: `${currentPage / totalPage * 97}%` }
-                }>
-                    <View className='absolute right-0 top-0 transform 
-                    bg-white border-2 border-purple-600 rounded-full'
-                        style={[
-                            { width: 24, height: 24},
-                            { transform: [ { translateX: 12 }, { translateY: -10 } ] }
-                        ]}
-                    />
+                <View className='bg-purple-500 h-1 w-1/2' style={{ width: 12 }}/>
+                <View className='flex-1 z-10 bg-slate-300'>
+                    <View className='bg-purple-500 h-1 w-1/2 relative z-10' style={
+                        { width: `${(currentPage - 1) / (totalPage - 1) * 100}%` }
+                    }>
+                        <View className='absolute right-0 top-0 transform 
+                        bg-white border-2 border-purple-600 rounded-full'
+                            style={[
+                                { width: 24, height: 24},
+                                { transform: [ { translateX: 12 }, { translateY: -10 } ] }
+                            ]}
+                        />
+                    </View>
                 </View>
-                <View className='bg-gray-300 h-1 w-1/2 flex-1' />
+                <View className='bg-gray-300 h-1' style={{ minWidth: 12 }}/>
             </View>
             <View className='bg-slate-50 w-full z-10' style={{ height: 50 }}>
                 <Text className='text-lg'>뷰어 푸터 { currentPage } / { totalPage }</Text>
---
+++ src/components/ViewerMenu.tsx
@@ -0,0 +1,47 @@
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import IParamList from "../models/interface/IParamList";
+import Icon from "react-native-vector-icons/FontAwesome";
+import orientationState from "../recoil/atom/orientationState";
+import { useRecoilState, useRecoilValue } from "recoil";
+import Nav from "./Nav";
+import ViewerFooter from "./ViewerFooter";
+import { navOpenState } from "../recoil/atom/viewerState";
+
+export default function ViewerMenu() {
+
+    const navigation = useNavigation<NavigationProp<IParamList>>();
+    const isLandscape = useRecoilValue(orientationState);
+    const [navOpen, setNavOpen] = useRecoilState(navOpenState);
+
+    const leftButton = (
+        <TouchableOpacity onPress={() => {
+            navigation.canGoBack() ? navigation.goBack() : navigation.navigate('Home');
+        }}>
+            <Icon name='chevron-left' size={20} color='#9333ea'></Icon>
+        </TouchableOpacity>
+    );
+
+    const rightButton = (
+        <TouchableOpacity onPress={() => { setNavOpen(!navOpen) }}>
+            <Text className='font-bold text-purple-600'>숨기기</Text>
+        </TouchableOpacity>
+    );
+
+    return (
+        <>
+            <SafeAreaView pointerEvents={ navOpen ? 'auto' : 'none' }
+                className={`absolute w-full bg-slate-50 border-b border-gray-200 ${navOpen ? '' : 'opacity-0'}`}
+            >
+                <View><Nav leftButton={leftButton} title={'이름'} rightButton={rightButton}/></View>
+            </SafeAreaView>
+            <SafeAreaView pointerEvents={ navOpen ? 'auto' : 'none' }
+                className={`absolute bottom-0 w-full bg-slate-50 border-t border-gray-200 ${navOpen ? '' : 'opacity-0'}`}
+            >
+                <View className='w-full relative'>
+                    <ViewerFooter/>
+                </View>
+            </SafeAreaView>
+        </>
+    );
+}
